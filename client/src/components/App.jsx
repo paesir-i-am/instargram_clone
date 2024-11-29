e95eac3e917a5db07b02831ce6cded0db66dd6b5
paesir <jay6423@gmail.com>
2024-11-29 10:34:56 +0900

    no message

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import '@styles/core.css';
-import Welcome from './Welcome/Welcome';
 import Login from './Login/login';
 import Join from './Join/join';
 
--- client/src/components/Join/join.jsx
+++ client/src/components/Join/join.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './css/join.css'
 
 function Join() {
   return (
