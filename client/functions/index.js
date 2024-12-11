43f3c3bf883a08ff41609b9486f7afcf05d18046
paesir <jay6423@gmail.com>
2024-12-11 19:07:51 +0900

    no message

--- client/functions/index.js
+++ client/functions/index.js
@@ -6,14 +6,11 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
-
+const functions = require("firebase-functions");
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const clientApi = require('./router/client')
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+exports.clientApi = functions.https.onRequest(clientApi);
--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import './css/login.css'
+import React, { useCallback, useState } from 'react';
+import './css/login.css';
+import firebaseApp from '@config/firebaseApp';
+
+const Fauth = firebaseApp.auth();
 
 function Login() {
+
+  const [email, setEmail] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
+  const __doLogin = useCallback(
+    (e) => {
+      e.preventDefault();
+      Fauth.signInWithEmailAndPassword(email, password).then(credential => {
+        const { user } = credential;
+        console.log(user);
+      }).catch(error => {
+        console.log(error);
+      });
+    },
+    [email, password]
+  );
+
+  const __logout = useCallback(()=>{
+    Fauth.signOut().then(() => {
+      console.log('logout complete')
+    }).catch(err=>{
+      console.log(err);
+    })
+  },[])
+
   return (
     <div className="login">
       <div className="wrapper">
         <div className="logo">
           <img src="/assets/welcome/logo.svg" alt="logo" />
         </div>
-        <form className="login-contents">
+        <form className="login-contents" onSubmit={__doLogin}>
           <div className="email-inp custom-inp">
             <div className="title txt-bold">이메일</div>
             <div className="inp">
-              <input type="email" placeholder="이메일을 입력해주세요" required />
+              <input type="email" placeholder="이메일을 입력해주세요"
+                     onBlur={(e) => setEmail(e.target.value)}
+                     required />
             </div>
           </div>
           <div className="password-inp custom-inp">
             <div className="title txt-bold">비밀번호</div>
             <div className="inp">
-              <input type="password" placeholder="비밀번호를 입력해주세요" required />
+              <input type="password" placeholder="비밀번호를 입력해주세요"
+                     onBlur={(e) => setPassword(e.target.value)}
+                     required />
             </div>
           </div>
           <button className="login-btn" type="submit">로그인 하기</button>
